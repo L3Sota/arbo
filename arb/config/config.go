387d02cc626be87d95f07d6e4fab34367349b76c
L3Sota <l3sota@pm.me>
2024-01-16 16:41:45 +0900

    feat(config): load singleton

--- arb/config/config.go
+++ arb/config/config.go
@@ -1,7 +1,22 @@
 package config
 
+import "github.com/kelseyhightower/envconfig"
+
 type Config struct {
 	PEnable bool   `split_words:"true"`
 	PKey    string `split_words:"true"`
 	PUser   string `split_words:"true"`
+
+	loaded bool
+}
+
+var c Config
+
+func Load() *Config {
+	if !c.loaded {
+		envconfig.MustProcess("ARBO", &c)
+		c.loaded = true
+	}
+
+	return &c
 }
--- arb/main/main.go
+++ arb/main/main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"github.com/L3Sota/arbo/arb"
 	"github.com/L3Sota/arbo/arb/config"
-	"github.com/kelseyhightower/envconfig"
 )
 
 func main() {
-	c := &config.Config{}
-	envconfig.MustProcess("ARBO", c)
-
-	arb.Book(c)
+	arb.Book(config.Load())
 }
