43a56cffd31eb6af750ad808b2c455631b8958cc
L3Sota <l3sota@pm.me>
2024-02-14 23:19:39 +0900

    feat(h): book

--- go.mod
+++ go.mod
@@ -14,9 +14,15 @@ require (
 	gopkg.in/resty.v1 v1.12.0
 )
 
+require github.com/huobirdcenter/huobi_golang v0.0.0-20210226095227-8a30a95b6d0d
+
+require (
+	github.com/chuckpreslar/emission v0.0.0-20170206194824-a7ddd980baf9 // indirect
+	github.com/orcaman/concurrent-map/v2 v2.0.1 // indirect
+)
+
 require (
 	github.com/antihax/optional v1.0.0 // indirect
-	github.com/chuckpreslar/emission v0.0.0-20170206194824-a7ddd980baf9 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible // indirect
@@ -25,10 +31,9 @@ require (
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/konsorten/go-windows-terminal-sequences v1.0.1 // indirect
 	github.com/leodido/go-urn v1.2.3 // indirect
-	github.com/orcaman/concurrent-map/v2 v2.0.1 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/sirupsen/logrus v1.4.1 // indirect
 	github.com/valyala/fastjson v1.6.4 // indirect
-	golang.org/x/net v0.0.0-20181220203305-927f97764cc3 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 )
--- h/h.go
+++ h/h.go
@@ -1,13 +1,60 @@
-package main
+package h
 
 import (
 	"fmt"
 	"log/slog"
 
+	"github.com/L3Sota/arbo/arb/model"
+	"github.com/huobirdcenter/huobi_golang/config"
+	"github.com/huobirdcenter/huobi_golang/pkg/client"
+	"github.com/huobirdcenter/huobi_golang/pkg/model/market"
 	"github.com/linstohu/nexapi/htx/spot/marketws"
+	"github.com/shopspring/decimal"
 )
 
-func main() {
+var (
+	// TODO
+	AskAddition  = decimal.NewFromInt(1)
+	BidReduction = decimal.NewFromInt(1)
+
+	mc *client.MarketClient
+)
+
+func LoadClient() {
+	mc = new(client.MarketClient).Init(config.Host)
+}
+
+func Book() ([]model.Order, []model.Order, error) {
+	o, err := mc.GetDepth("xchusdt", "step0", market.GetDepthOptionalRequest{})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	a := make([]model.Order, 0, len(o.Asks))
+	for _, ask := range o.Asks {
+		o := model.Order{
+			Ex:     model.ExchangeTypeHu,
+			Price:  ask[0],
+			Amount: ask[1],
+		}
+		o.EffectivePrice = o.Price.Mul(AskAddition)
+		a = append(a, o)
+	}
+	b := make([]model.Order, 0, len(o.Bids))
+	for _, bid := range o.Bids {
+		o := model.Order{
+			Ex:     model.ExchangeTypeHu,
+			Price:  bid[0],
+			Amount: bid[1],
+		}
+		o.EffectivePrice = o.Price.Mul(BidReduction)
+		b = append(b, o)
+	}
+
+	return a, b, nil
+}
+
+func WSTest() {
 	// c := &client.MarketClient{}
 	// c.Init(config.Host)
 
